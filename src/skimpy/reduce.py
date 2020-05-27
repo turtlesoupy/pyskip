4962e902525784f5af095d9bc849f4e2148df84a
Taylor Gordon <taylorgordon20@gmail.com>
2020-05-27 12:31:22 -0700

    Update formatting

--- src/skimpy/reduce.py
+++ src/skimpy/reduce.py
@@ -9,28 +9,28 @@ def reduce(
     op: Callable[[Scalar, Scalar], Scalar],
     t: Tensor,
     axis: int = None,
-    keepdims = False,
+    keepdims=False,
 ) -> Union[Tensor, Scalar]:
-  if axis is None:
-    return reduce(op, flatten(t), axis = 0, keepdims = keepdims)
-  assert 0 <= axis < len(t.shape)
-  if len(t) == 1:
-    return t if keepdims else t.item()
-  if t.shape[axis] == 1:
-    return t if keepdims else squeeze(t)
-  else:
-    n = t.shape[axis]
-    lo = take(t, slice(None, n // 2), axis)
-    hi = take(t, slice(n // 2, None), axis)
-    return reduce(op, op(lo, hi), axis, keepdims)
+    if axis is None:
+        return reduce(op, flatten(t), axis=0, keepdims=keepdims)
+    assert 0 <= axis < len(t.shape)
+    if len(t) == 1:
+        return t if keepdims else t.item()
+    if t.shape[axis] == 1:
+        return t if keepdims else squeeze(t)
+    else:
+        n = t.shape[axis]
+        lo = take(t, slice(None, n // 2), axis)
+        hi = take(t, slice(n // 2, None), axis)
+        return reduce(op, op(lo, hi), axis, keepdims)
 
 
-def add(t: Tensor, axis: int = None, keepdims = False) -> Tensor:
-  return reduce(operator.add, t, axis, keepdims)
+def add(t: Tensor, axis: int = None, keepdims=False) -> Tensor:
+    return reduce(operator.add, t, axis, keepdims)
 
 
-def mul(t: Tensor, axis: int = None, keepdims = False) -> Tensor:
-  return reduce(operator.mul, t, axis, keepdims)
+def mul(t: Tensor, axis: int = None, keepdims=False) -> Tensor:
+    return reduce(operator.mul, t, axis, keepdims)
 
 
 # Convenience aliases for reduction operators
--- src/skimpy/tests/convolve_test.py
+++ src/skimpy/tests/convolve_test.py
@@ -6,32 +6,32 @@ import skimpy.functional as F
 
 
 class TestConvolve(TestCase):
-  @classmethod
-  def make_disc(cls):
-    D = 4096
-    R = D // 4
-    disc = skimpy.Tensor((D, D), 0)
-    for y in range(D):
-      discriminant = R**2 - (y - D // 2 + 0.5)**2
-      if discriminant < 0:
-        continue
-      x_0 = int(D // 2 - 0.5 - discriminant**0.5)
-      x_1 = int(D // 2 - 0.5 + discriminant**0.5)
-      disc[x_0:x_1, y] = 1
-    return disc
+    @classmethod
+    def make_disc(cls):
+        D = 4096
+        R = D // 4
+        disc = skimpy.Tensor((D, D), 0)
+        for y in range(D):
+            discriminant = R**2 - (y - D // 2 + 0.5)**2
+            if discriminant < 0:
+                continue
+            x_0 = int(D // 2 - 0.5 - discriminant**0.5)
+            x_1 = int(D // 2 - 0.5 + discriminant**0.5)
+            disc[x_0:x_1, y] = 1
+        return disc
 
-  def test_conv_2d(self):
-    disc = TestConvolve.make_disc()
-    sobel = np.array([
-        [-1, 0, 1],
-        [-2, 0, 2],
-        [-1, 0, 1],
-    ])
+    def test_conv_2d(self):
+        disc = TestConvolve.make_disc()
+        sobel = np.array([
+            [-1, 0, 1],
+            [-2, 0, 2],
+            [-1, 0, 1],
+        ])
 
-    edge = F.conv_2d(
-        disc,
-        skimpy.Tensor.from_numpy(sobel),
-        padding = 1,
-    )
+        edge = F.conv_2d(
+            disc,
+            skimpy.Tensor.from_numpy(sobel),
+            padding=1,
+        )
 
-    self.assertEqual(F.sum(edge.abs()), 32768)
+        self.assertEqual(F.sum(edge.abs()), 32768)
