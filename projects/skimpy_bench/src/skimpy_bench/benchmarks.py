708489c2c2360fbc13d6b0aef14a8030faef7ace
Thomas Dimson <tdimson@gmail.com>
2020-05-30 11:30:44 -0700

    Sparse array benchmark

--- projects/skimpy_bench/src/skimpy_bench/benchmarks.py
+++ projects/skimpy_bench/src/skimpy_bench/benchmarks.py
@@ -1,6 +1,7 @@
 import gc
 import time
 import numpy as np
+import random
 import functools
 import _skimpy_cpp_ext
 from _skimpy_bench_cpp_ext import taco, memory
@@ -46,20 +47,37 @@ class Timer:
 
 class Benchmark:
     @classmethod
-    def run_against_axis(cls, varying_arg_name, varying_arg, repeats, **other_args):
+    def run_against_axis(cls, varying_arg_name, varying_arg, repeats, verbose=False, **other_args):
         ret = {}
         for val in varying_arg:
             args = {
                 varying_arg_name: val,
                 **other_args
             }
-            times = cls(**args).run(repeats=repeats)
+            times = cls(**args).run(repeats=repeats, verbose=verbose)
             for k, v in times.items():
                 if k in ret:
                     ret[k].append(v)
                 else:
                     ret[k] = [v]
         return {k: np.array(v) for k, v in ret.items()}
+
+    def run(self, repeats=3, verbose=False):
+        ret = defaultdict(list)
+        for i in range(repeats):
+            if verbose:
+                print(f"{self.__class__.__name__} running skimpy")
+            ret["skimpy"].append(self.run_skimpy(**self.suite_kwargs.get("skimpy", {})))
+            if verbose:
+                print(f"{self.__class__.__name__} running taco")
+            ret["taco"].append(self.run_taco(**self.suite_kwargs.get("taco", {})))
+            if verbose:
+                print(f"{self.__class__.__name__} running memory")
+            ret["memory"].append(self.run_memory(**self.suite_kwargs.get("memory", {})))
+            if verbose:
+                print(f"{self.__class__.__name__} running numpy")
+            ret["numpy"].append(self.run_numpy(**self.suite_kwargs.get("numpy", {})))
+        return {k: np.mean(np.array(v)) for k, v in ret.items()}
 
 
 class DenseArrayBenchmark(Benchmark):
@@ -101,29 +119,67 @@ class DenseArrayBenchmark(Benchmark):
             num_threads=4,
         ) * MICR_TO_MS
 
-    def run(self, repeats=3):
-        ret = defaultdict(list)
-        for i in range(repeats):
-            ret["skimpy"].append(self.run_skimpy(**self.suite_kwargs.get("skimpy", {})))
-            ret["taco"].append(self.run_taco(**self.suite_kwargs.get("taco", {})))
-            ret["memory"].append(self.run_memory(**self.suite_kwargs.get("memory", {})))
-            ret["numpy"].append(self.run_numpy(**self.suite_kwargs.get("numpy", {})))
-        return {k: np.mean(np.array(v)) for k, v in ret.items()}
-
 
 class DenseConvolutionBenchmark:
     pass
 
 
-class SparseArrayBenchmark:
-    pass
+class RunLengthArrayBenchmark(Benchmark):
+    def __init__(self, array_length, num_non_zero, max_run_length, align_inputs, num_inputs, suite_kwargs=None):
+        self.array_length = array_length
+        self.num_non_zero = num_non_zero
+        self.max_run_length = max_run_length
+        self.align_inputs = align_inputs
+        self.num_inputs = num_inputs
+        self.suite_kwargs = suite_kwargs or {}
+
+    def run_numpy(self):
+        inputs = [np.random.randint(2**3, size=self.array_length) for _ in range(self.num_inputs)]
+        t = Timer()
+        with t:
+            _ = functools.reduce(lambda x, y: x + y, inputs)
+        return t.duration_ms
+
+    def run_skimpy(self, num_threads=1):
+        inputs = []
+        for i in range(self.num_inputs):
+            builder = _skimpy_cpp_ext.IntBuilder(self.array_length, 0)
+            if self.align_inputs:
+                random.seed(42)
+
+            for j in range(self.num_non_zero):
+                insert_position = random.randint(0, self.array_length)
+                run_length = random.randint(1, self.max_run_length)
+                val = random.randint(0, 2 ** 16)
+                builder[insert_position:(insert_position + run_length)] = val
+            inputs.append(builder.build())
+
+        with num_threads_scope(num_threads):
+            t = Timer()
+            with t:
+                _ = functools.reduce(lambda x, y: x + y, inputs).eval()
+
+        return t.duration_ms
+
+    def run_taco(self):
+        return taco.sparse_sum(
+            num_elements=self.array_length,
+            num_non_zero=self.num_non_zero,
+            max_run_length=self.max_run_length,
+            align_inputs=self.align_inputs,
+            num_input_arrays=self.num_inputs,
+            include_compile_time=False,
+        ) * MICR_TO_MS
+
+    def run_memory(self):
+        return memory.no_simd_int_cum_sum_write(
+            num_elements=self.num_non_zero * 2,  # Sparse is assumed to be a list of (pos, val)
+            num_input_arrays=self.num_inputs,
+            num_threads=4,
+        ) * MICR_TO_MS
 
 
 class SparseConvolutionBenchmark:
-    pass
-
-
-class RunLengthArrayBenchmark:
     pass
 
 
