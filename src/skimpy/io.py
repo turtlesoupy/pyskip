e3868a2ed6ea328f3fb5639ac645961c8e252432
Taylor Gordon <taylorgordon20@gmail.com>
2020-06-02 19:41:37 -0700

    Fix bug in tensor string formatting

--- src/skimpy/io.py
+++ src/skimpy/io.py
@@ -12,7 +12,7 @@ def format_tensor(tensor: Tensor, threshold: int = 20, separator: str = " "):
     truncated = tensor
     for i, l in enumerate(tensor.shape):
         if l > threshold:
-            head = take(truncated.ndim, slice(threshold), axis=i)
-            tail = take(truncated.ndim, slice(-threshold, None), axis=i)
+            head = take(truncated, slice(threshold), axis=i)
+            tail = take(truncated, slice(-threshold, None), axis=i)
             truncated = stack(head, tail)
     return np.array2string(truncated.to_numpy(), threshold=threshold, separator=separator)
