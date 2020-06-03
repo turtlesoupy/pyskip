75a28e885f3885570019632f86c056d8e4c2b1d8
Taylor Gordon <taylorgordon20@gmail.com>
2020-06-02 19:41:22 -0700

    Add skimpy_blox install requires

--- projects/skimpy_blox/setup.py
+++ projects/skimpy_blox/setup.py
@@ -5,6 +5,6 @@ setup(
     version="0.0.1",
     author="Taylor Gordon, Thomas Dimson",
     description="A skimpy wrapper library for loading and operating on Minecraft assets",
-    packages=['skimpy_blox'],
-    install_requires=['skimpy>=0.0.1'],
+    packages=["skimpy_blox"],
+    install_requires=["skimpy>=0.0.1", "nbt", "tqdm"],
 )
